f2f5596e9f17ca4bb20072adfb67729cb70e2fe1
Nimo Beeren <nimobeeren@gmail.com>
2019-10-31 22:46:47 +0100

    Fix tests

--- client/src/components/__tests__/App.test.tsx
+++ client/src/components/__tests__/App.test.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import App from '../App';
-import { Counter } from '../Counter';
+import { FileList } from '../FileList';
 
 describe('<App />', () => {
   test('renders without exploding', () => {
-    const wrapper = mount(<App />);
+    const wrapper = shallow(<App />);
     expect(wrapper).not.toBeNull();
   });
 
   test('renders a <Counter />', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.is(Counter)).toBe(true);
+    expect(wrapper.find(FileList)).toHaveLength(1);
   });
 });
--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -2,3 +2,5 @@ const { configure: configureEnzyme } = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
 configureEnzyme({ adapter: new Adapter() });
+
+global.fetch = () => undefined;
