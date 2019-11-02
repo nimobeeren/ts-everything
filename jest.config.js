3862e8a489dfa1f0d443d3bad3841dda6dc2903b
Nimo Beeren <nimobeeren@gmail.com>
2019-11-02 21:16:00 +0100

    Move generated code to a separate directory

--- jest.config.js
+++ jest.config.js
@@ -32,7 +32,10 @@ module.exports = {
   // coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ['/node_modules/'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/__generated__/'
+  ],
 
   // A list of reporter names that Jest uses when writing coverage reports
   coverageReporters: ['html', 'text-summary'],
