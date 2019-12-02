ba6c3e945345a1815433b63a3aae6e5a19922b4a
Nimo Beeren <nimo@deity.io>
2019-12-02 22:51:33 +0100

    Add meaningful tests

--- test-utils/src/__tests__/ApolloMockingProvider.test.tsx
+++ test-utils/src/__tests__/ApolloMockingProvider.test.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { ApolloConsumer } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-client';
+import { IMocks } from 'graphql-tools';
 import { ApolloMockingProvider } from '../ApolloMockingProvider';
+
+// Mock the schema so we are really just testing the component itself
+jest.mock('../../../graphql/src/schema.graphql');
 
 describe('<ApolloMockingProvider />', () => {
   it('renders without exploding', () => {
     const wrapper = mount(<ApolloMockingProvider />);
     expect(wrapper.is(ApolloMockingProvider)).toBe(true);
+  });
+
+  it('renders its children', () => {
+    const wrapper = mount(
+      <ApolloMockingProvider>
+        <div id="child" />
+      </ApolloMockingProvider>
+    );
+    expect(wrapper.find('#child')).toHaveLength(1);
+  });
+
+  it('provides Apollo context', done => {
+    const mocks: IMocks = {
+      Foo: () => ({
+        items: []
+      })
+    };
+
+    mount(
+      <ApolloMockingProvider mocks={mocks}>
+        <ApolloConsumer>
+          {client => {
+            expect(client).toBeInstanceOf(ApolloClient);
+            done();
+            return null;
+          }}
+        </ApolloConsumer>
+      </ApolloMockingProvider>
+    );
   });
 });
