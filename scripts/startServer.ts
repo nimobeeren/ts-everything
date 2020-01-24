68a36d07f0102dd4ecfa130f2ffabae9021f2688
Nimo Beeren <nimo@deity.io>
2020-01-24 21:21:35 +0100

    Clean up start server script

--- scripts/startServer.ts
+++ scripts/startServer.ts
@@ -2,40 +2,25 @@ import path from 'path';
 import { fork, ChildProcess } from 'child_process';
 import Bundler from 'parcel-bundler';
 
-async function clientDev() {
-  const clientEntryFile = path.resolve(__dirname, '../client/src/index.html');
-  const options: Bundler.ParcelOptions = {
-    outDir: path.resolve(__dirname, '../client/dist')
-  };
-  const bundler = new Bundler(clientEntryFile, options);
-  return bundler.serve();
-}
+const serverEntryFile = path.resolve(__dirname, '../server/src/index.ts');
+const serverOutFile = path.resolve(__dirname, '../server/dist/index.js');
+const options: Bundler.ParcelOptions = {
+  outDir: path.dirname(serverOutFile),
+  target: 'node'
+};
 
-async function serverDev() {
-  const serverEntryFile = path.resolve(__dirname, '../server/src/index.ts');
-  const options: Bundler.ParcelOptions = {
-    outDir: path.resolve(__dirname, '../server/dist'),
-    target: 'node'
-  };
-  const bundler = new Bundler(serverEntryFile, options);
-  bundler.bundle();
+const bundler = new Bundler(serverEntryFile, options);
+bundler.bundle();
 
-  let serverProc: ChildProcess = null;
-  const serverOutFile = path.resolve(__dirname, '../server/dist/index.js');
-
-  bundler.on('bundled', () => {
-    if (!serverProc) {
+let serverProc: ChildProcess = null;
+bundler.on('bundled', () => {
+  if (!serverProc) {
+    serverProc = fork(serverOutFile);
+  } else {
+    console.log('Restarting...');
+    serverProc.kill();
+    serverProc.on('exit', () => {
       serverProc = fork(serverOutFile);
-    } else {
-      console.log('Restarting...');
-      serverProc.kill();
-      serverProc.on('exit', () => {
-        serverProc = fork(serverOutFile);
-      });
-    }
-  });
-}
-
-(async () => {
-  await serverDev();
-})();
+    });
+  }
+});
