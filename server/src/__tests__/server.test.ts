61bbb8b05fc458717048eed7d449cbfbb9f41af8
Nimo Beeren <nimo@deity.io>
2020-03-19 20:09:59 +0100

    merge server integration and unit tests

--- server/src/__tests__/server.test.ts
+++ server/src/__tests__/server.test.ts
@@ -1,23 +1,33 @@
-import { Server } from 'http';
+import { ApolloServer } from 'apollo-server-koa';
+import { createTestClient } from 'apollo-server-testing';
 import request from 'supertest';
+import typeDefs from '../../../graphql/src/schema.graphql';
+import { resolvers } from '../api';
+import { FileListDocument } from '../../../graphql/src';
 import { start } from '../server';
 
 jest.mock('../../../graphql/src/schema.graphql');
 jest.mock('../api');
 
 describe('Server', () => {
-  let server: Server;
-  beforeEach(() => {
-    server = start(process.env.PORT || 4000);
-  });
-
-  afterEach(() => {
+  it('responds to HTTP requests', async () => {
+    const server = start(process.env.PORT || 4000);
+    const response = await request(server).get('/');
+    expect(response).not.toBeNull();
     server.close();
   });
 
-  it('responds to HTTP requests', async () => {
-    const response = await request(server).get('/');
-    expect(response).not.toBeNull();
+  it('fetches file list', async () => {
+    // create a test server to test against, using our production typeDefs,
+    // resolvers, and dataSources.
+    const server = new ApolloServer({ typeDefs, resolvers });
+
+    // use the test server to create a query function
+    const { query } = createTestClient(server);
+
+    // run query against the server and snapshot the output
+    const res = await query({ query: FileListDocument });
+    expect(res).toMatchSnapshot();
   });
 
   // TODO: test some GraphQL requests
